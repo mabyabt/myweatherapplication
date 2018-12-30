a72dd5323876148ef531fd44f0197f972524aac4
Matuz Bence <matuzb97@gmail.com>
2018-12-30 16:05:23 +0100

    url builder

---
+++ MyWeatherApp01/MyWeatherApp01/WebRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWeatherApp01
+{
+    class WebRequest
+    {
+        private string urlFirst = "https://api.openweathermap.org/data/2.5/weather?q=";
+        private string apiKey = "2b8c4f840e3bb8352ca8c2d767e82d17";
+        private string urlCenter = "&appid=";
+        private string urlEnd = "&mode=xml";
+        
+
+        public  string getUrl(string city) {
+             string finishedUrl ;
+
+            return finishedUrl= urlFirst+city+urlCenter+apiKey+urlEnd;
+        }
+
+
+    }
+}
