087a7c06619b1bd2ea78cc261ecee9aac68d72ea
Matuz Bence <matuzb97@gmail.com>
2019-01-01 16:54:44 +0100

    Update WebRequest.cs

--- MyWeatherApp01/MyWeatherApp01/WebRequest.cs
+++ MyWeatherApp01/MyWeatherApp01/WebRequest.cs
@@ -7,7 +7,7 @@ namespace MyWeatherApp01
     class WebRequest
     {
         private string urlFirst = "https://api.openweathermap.org/data/2.5/weather?q=";
-        private string apiKey = "2b8c4f840e3bb8352ca8c2d767e82d17";
+        private string apiKey = "____openweatherapi_key____";
         private string urlCenter = "&appid=";
         private string urlEnd = "&mode=xml";
         
